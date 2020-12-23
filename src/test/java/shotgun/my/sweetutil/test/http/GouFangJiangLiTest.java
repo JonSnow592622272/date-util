d3d1ca24f141847639fda86de48ddbc6b5818794
wulingming <592622272@qq.com>
2020-12-23 20:29:01 +0800

    完善

---
+++ src/test/java/shotgun/my/sweetutil/test/http/GouFangJiangLiTest.java
@@ -0,0 +1,45 @@
+package shotgun.my.sweetutil.test.http;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 契税奖励请求
+ **/
+public class GouFangJiangLiTest {
+
+    public static ExecutorService pool = Executors.newFixedThreadPool(16);
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        asaynRun(1000, () -> {
+                    System.out.println("娃哈哈");
+                }, () -> {
+                    System.out.println("娃哈哈222");
+                }
+
+        );
+    }
+
+    public static void asaynRun(long intervalMillis, Runnable... runnables) throws InterruptedException {
+        if (runnables == null || runnables.length == 0) {
+            return;
+        }
+        pool.execute(() -> {
+            while (true) {
+                for (Runnable runnable : runnables) {
+                    pool.execute(runnable);
+                }
+                try {
+                    Thread.sleep(intervalMillis);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+}
